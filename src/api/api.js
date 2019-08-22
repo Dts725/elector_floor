8e74a0b4e61610e17be1de062b4b7c2f4275c095
tao <tadbao@163.com>
2019-08-22 18:49:46 +0800

    添加登录失效

--- src/api/api.js
+++ src/api/api.js
@@ -6,6 +6,19 @@ axios.defaults.baseURL = 'https://apijcdj.shyunhua.com';
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
     // 对响应数据做点什么
+
+    if (response.data.code == 203) {
+        this.$message({
+            type: "error",
+            message: response.status
+        });
+
+
+        this.$router.push('/login')
+        return true
+
+    }
+
     return response;
 }, function (error) {
     // 对响应错误做点什么
