85c51942f278aa805a9e5cc56832899720134407
taodabao <tadbao@163.com>
2019-08-26 21:11:15 +0800

    合并

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  // base: 'floor_map',
+  // base: 'floor_map', // 此处该或不该都可以 此处打包路径 继承 vue.config.js    publicPath: '/floor_map/', 
   routes: [
 
 
